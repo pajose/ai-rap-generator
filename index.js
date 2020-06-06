3588c4b64d698facb01ae1f7b3b9e4b079efebc7
Patrick Jose <patrickgjose@gmail.com>
2020-06-06 15:56:23 -0700

    pls work

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 
-var http = require('http');
-var server = http.Server(app);
+// var http = require('http');
+// var server = http.Server(app);
 
 //app.use(express.static('client'));
 
