fe8a86f51e5d46f6eb722cdd556be12ead080caf
Patrick Jose <patrickgjose@gmail.com>
2020-06-06 16:06:07 -0700

    Changed app to server in index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 
-// var http = require('http');
-// var server = http.Server(app);
+var http = require('http');
+var server = http.Server(app);
 
-//app.use(express.static('client'));
 
 app.get("/", function(req, res) {
 	res.render("index");
@@ -13,13 +12,5 @@ app.get("/", function(req, res) {
 app.use(express.static(__dirname));
 
 server.listen(PORT, function() {
-  console.log('Patrick\'s app running');
+  console.log('Patrick app running');
 });
-
-// var io = require('socket.io')(server);
-
-// io.on('connection', function(socket) {
-//   socket.on('message', function(msg) {
-//     io.emit('message', msg);
-//   });
-// });
