8ce45232ce406af910bef32f8ef62a2b466d4396
Patrick Jose <patrickgjose@gmail.com>
2020-06-06 16:02:37 -0700

    Reverted index.js to previous version to see if that is issue

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-
-var express = require('express')
+var PORT = process.env.PORT || 5000;
+var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
+// var http = require('http');
+// var server = http.Server(app);
 
+//app.use(express.static('client'));
+
+app.get("/", function(req, res) {
+	res.render("index");
+})
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res) {
-	res.render('index');
-})
+server.listen(PORT, function() {
+  console.log('Patrick\'s app running');
+});
 
-app.listen(port, function() {
-	console.log('app running')
-})
+// var io = require('socket.io')(server);
+
+// io.on('connection', function(socket) {
+//   socket.on('message', function(msg) {
+//     io.emit('message', msg);
+//   });
+// });
