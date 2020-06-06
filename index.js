63e23d857da8bbbaeca87add941bd334afb93b45
Patrick Jose <patrickgjose@gmail.com>
2020-06-06 16:12:02 -0700

    AI rap generator initialized

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-var PORT = process.env.PORT || 5000;
-var express = require('express');
+var express = require('express')
 var app = express();
 
-var http = require('http');
-var server = http.Server(app);
+var port = process.env.PORT || 8080;
 
-
-// app.get("/", function(req, res) {
-// 	res.render("index");
-// })
 app.use(express.static(__dirname));
 
-server.listen(PORT, function() {
-  console.log('Patrick app running');
-});
+app.get('/', function(req, res) {
+	res.render('index');
+})
 
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
+app.listen(port, function() {
+	console.log('AI rap generator running')
+})
