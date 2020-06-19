64753f249ed8f78a8cfdb3b9ebee41fcb5825847
Christian Poon <christianxp27@yahoo.com>
2020-06-18 18:11:26 -0700

    runs python script and extracts text file

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-var express = require('express')
-var app = express();
+const express = require('express');
+const app = express();
+const fs = require('fs');
+let {PythonShell} = require('python-shell');
+const e = require('express');
+let options = { options: ['-u'], scriptPath: './python' }
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
+// Run the python script at the root
+app.get('/', (req, res) => {
 	res.render('index');
+	PythonShell.run('main.py', options, function (err) {
+		if (err) throw err;
+		console.log('Finished running the code');
+	});
 })
 
-app.listen(port, function() {
-	console.log('AI rap generator running')
+// Visit this route to view the contents of the rap file
+app.get('/generated', (req, res) => {
+	const filename = 'kendrick_lamar_neural_network_rap.txt';
+	fs.readFile(filename, 'utf8', (e, data) => {
+			if (e) throw e;
+			console.log('OK: ' + filename);
+  		console.log(data)
+			res.write(data);
+			res.end();
+	});
+});
+
+app.listen(port, () => {
+	console.log('AI rap generator running on port ' + port);
 })
