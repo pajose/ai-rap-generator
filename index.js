73003ffb0230533dd02938481c3af1edd354ad2a
Patrick Jose <patrickgjose@gmail.com>
2020-06-23 22:31:54 -0700

    Merged files. Updated index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 let {PythonShell} = require('python-shell');
-const e = require('express');
 let options = { options: ['-u'], scriptPath: './python' }
 
 const port = process.env.PORT || 8080;
 
+// Set static folder
 app.use(express.static(__dirname + "/public"));
+
 app.set("view engine", "ejs");
 
 // Run the python script at the root
@@ -16,8 +17,11 @@ app.get('/', (req, res) => {
 	// 	if (err) throw err;
 	// 	console.log('Finished running the code');
 	// });
+	fs.readFile("kendrick_lamar_base_markov_rap.txt", "utf-8", (err, data) => {
+		console.log(typeof data);
+		res.render('index', {bars : data});
+	})
 
-	res.render('index');
 })
 
 // Visit this route to view the contents of the rap file
